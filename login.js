c676eab5325116c7a6afe01f0a90ff8f63c0838f
Navneet <93375038+MrNavneetChauhan@users.noreply.github.com>
2022-10-11 11:30:59 +0530

    Update login.js

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ var search = document.getElementById("search");
 var navLastDiv = document.getElementById("navLastDiv");
 var logo = document.getElementById("logoId");
 var section_box = document.querySelector(".section-box");
-var imageDiv = document.querySelector(".imageDiv")
+var imageDiv = document.querySelector(".imageDiv");
 var tgl2 = document.querySelector(".tgl2");
 var toggleBar = document.querySelector(".toggleBar");
 var tglFwd = document.querySelector(".tglFwd");
@@ -14,116 +14,119 @@ var third = document.getElementById("third");
 var fourth = document.getElementById("fourth");
 var fifth = document.getElementById("fifth");
 var sixth = document.getElementById("sixth");
-var createbtn = document.getElementById("createbtn")
+var createbtn = document.getElementById("createbtn");
 var cart0 = document.getElementById("cart0");
 var help = document.getElementById("help");
 var zaraUserInfo = JSON.parse(localStorage.getItem("zaraUserInfo"));
 var loginBtn = document.getElementById("loginbtn");
 var email = document.getElementById("email");
 var password = document.getElementById("password");
-var p = document.getElementById("notif")
-var form  = document.querySelector("form");
-var userName = JSON.parse(localStorage.getItem("userName"))
-if(userName == null){
-  login.textContent = "LOGIN"
-}else{
-
-  login.textContent = userName
+var p = document.getElementById("notif");
+var form = document.querySelector("form");
+var userName = JSON.parse(localStorage.getItem("userName"));
+if (userName == null) {
+  login.textContent = "LOGIN";
+} else {
+  login.textContent = userName;
 }
 
-cartcnt.textContent = localStorage.getItem("count") || 0
+cartcnt.textContent = localStorage.getItem("count") || 0;
 
 //=============functionSelection====================//
 
-
-function activation(){
-    toggleBar.style.display = "block";
-    toggleBar2.style.display = "block"
-    tglFwd.style.display = "none";
-    console.log("hlo")
+function activation() {
+  toggleBar.style.display = "block";
+  toggleBar2.style.display = "block";
+  tglFwd.style.display = "none";
+  console.log("hlo");
 }
 
-function deactivation(){
-    toggleBar.style.display = "none";
-    tglFwd.style.display = "block";
-    toggleBar2.style.display = "none"
-    console.log("helo");
+function deactivation() {
+  toggleBar.style.display = "none";
+  tglFwd.style.display = "block";
+  toggleBar2.style.display = "none";
+  console.log("helo");
 }
 
-function goToWomenPage(){
-    console.log("hello")
-    window.location.href = "productDisplay.html"
-  }
-  function goToKidsPage(){
-      console.log("helo")
-  }
-  function goToLilPage(){
-      console.log("helo")
-  }
-  function goToManPage(){
-      console.log("helo")
-  }
-  function goToAthleticzPage(){
-      console.log("hleo")
-  }
-  function goToShoesBagsPage(){
-      console.log("hlo")
-  }
-  //========== toggle functions ends here============ //
+function goToWomenPage() {
+  console.log("hello");
+  window.location.href = "productDisplay.html";
+}
+function goToKidsPage() {
+  console.log("helo");
+}
+function goToLilPage() {
+  console.log("helo");
+}
+function goToManPage() {
+  console.log("helo");
+}
+function goToAthleticzPage() {
+  console.log("hleo");
+}
+function goToShoesBagsPage() {
+  console.log("hlo");
+}
+//========== toggle functions ends here============ //
 
-function goToCreatePage(){
-    window.location.href = "signup.html"
+function goToCreatePage() {
+  window.location.href = "signup.html";
 }
 
-function goToHome(){
-    window.location.href = "index.html"
+function goToHome() {
+  window.location.href = "index.html";
 }
 
-function goToSearch(){
-    window.location.href = "search.html"
+function goToSearch() {
+  window.location.href = "search.html";
 }
 
-function goToCartPage(){
-    window.location.href = "cart.html"
-    console.log("cart")
-  }
+function goToCartPage() {
+  window.location.href = "cart.html";
+  console.log("cart");
+}
 
-  
-  function goToHelpPage(){
-    window.location.href = "help.html"
-    console.log("hleo")
-  }
+function goToHelpPage() {
+  window.location.href = "help.html";
+  console.log("hleo");
+}
 
-  function goToUserPage(event){
-      event.preventDefault();
-      console.log("hi")
-      for(var i = 0; i < zaraUserInfo.length; i++){
-         if(zaraUserInfo[i].userEmail != email.value){
-            p.textContent = "please check your mail"
-         }else if(zaraUserInfo[i].userPassword != password.value){
-             p.textContent = "please check your password"
-         }else{
-             p.textContent = "";
-             localStorage.setItem("userName",JSON.stringify(zaraUserInfo[i].userName))
-             window.location.href = "index.html"
-         }
+function goToUserPage(event) {
+  event.preventDefault();
+  if (!zaraUserInfo) {
+    p.textContent = "Credentials are not registered, Please create account"
+  } else {
+    p.textContent = ""
+    for (var i = 0; i < zaraUserInfo.length; i++) {
+      if (zaraUserInfo[i].userEmail != email.value) {
+        p.textContent = "please check your mail";
+      } else if (zaraUserInfo[i].userPassword != password.value) {
+        p.textContent = "please check your password";
+      } else {
+        p.textContent = "";
+        localStorage.setItem(
+          "userName",
+          JSON.stringify(zaraUserInfo[i].userName)
+        );
+        window.location.href = "index.html";
+      }
     }
+  }
 }
 
 //=============eventsSelection====================//
-tgl2.addEventListener("click",deactivation);
+tgl2.addEventListener("click", deactivation);
 // toggleBar.addEventListener("click",print);
-tglFwd.addEventListener("click",activation);
-first.addEventListener("click",goToWomenPage);
-  second.addEventListener("click",goToManPage);
-  third.addEventListener("click",goToKidsPage);
-  fourth.addEventListener("click",goToLilPage);
-  fifth.addEventListener("click",goToShoesBagsPage);
-  sixth.addEventListener("click",goToAthleticzPage);
-  createbtn.addEventListener("click",goToCreatePage);
-  logo.addEventListener("click",goToHome);
-  search.addEventListener("click",goToSearch);
-  help.addEventListener("click",goToHelpPage);
-  cart0.addEventListener("click",goToCartPage);
-  form.addEventListener("submit",goToUserPage);
-
+tglFwd.addEventListener("click", activation);
+first.addEventListener("click", goToWomenPage);
+second.addEventListener("click", goToManPage);
+third.addEventListener("click", goToKidsPage);
+fourth.addEventListener("click", goToLilPage);
+fifth.addEventListener("click", goToShoesBagsPage);
+sixth.addEventListener("click", goToAthleticzPage);
+createbtn.addEventListener("click", goToCreatePage);
+logo.addEventListener("click", goToHome);
+search.addEventListener("click", goToSearch);
+help.addEventListener("click", goToHelpPage);
+cart0.addEventListener("click", goToCartPage);
+form.addEventListener("submit", goToUserPage);
